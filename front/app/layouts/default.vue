d35fcec2db9d647ac7c9591a6a34d558742d8ee4
Yassin97440 <yassinabdulla614@gmail.com>
2026-01-28 17:45:17 +0100

    init layout and app structure

---
+++ front/app/layouts/default.vue
@@ -0,0 +1,29 @@
+<!-- eslint-disable vue/no-multiple-template-root -->
+<!-- eslint-disable vue/html-indent -->
+<template>
+    <UHeader>
+        <template #left>
+            RIVO-IA
+        </template>
+        <template #right>
+            <UButton>
+                <template #icon>
+                    <UIcon name="i-lucide-user" />
+                </template>
+            </UButton>
+        </template>
+    </UHeader>
+    <UMain>
+        <slot />
+    </UMain>
+    <UFooter>
+        <template #left>
+            <p class="text-sm text-muted">
+                Built with Nuxt UI • © {{ new Date().getFullYear() }}
+            </p>
+        </template>
+    </UFooter>
+</template>
+
+<script setup>
+</script>
